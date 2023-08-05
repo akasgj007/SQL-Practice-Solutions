65ddb484a74dfc4de2177122b340a7254e6ef92a
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2023-08-05 14:20:16 +0530

    Update Easy_Queries.sql

--- db-Northwind/Easy/Easy_Queries.sql
+++ db-Northwind/Easy/Easy_Queries.sql
@@ -1,1 +1,18 @@
+-- Show the category_name and description from the categories table sorted by category_name.
+select
+  category_name,
+  description
+from categories
+order by category_name;
+
+
+
+
+
+
+
+
+
+
+
 
